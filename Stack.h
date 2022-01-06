7ca5396a9378b352f19d14c32e1b2d4a25130bec
Alejandro Arcucci <ale_arc2015@hotmail.com>
2022-01-05 21:00:56 -0300

    Generic Stack defined - No methods

---
+++ Stack.h
@@ -0,0 +1,26 @@
+#ifndef STACK_H_
+#define STACK_H_
+#include <iostream>
+#define MAX 10
+using namespace std;
+
+//Class definition
+template <class X>
+class Stack(){
+	private:
+		int top;
+		X* array;
+		int MAX;
+	public:
+		//Constructor
+		//Boolean verification
+		//Take out the item at the top of the stack
+		//Read the value of the item at the top of the stack
+		//Add an item to the top of the stack
+		//Get the number of items that are on the stack (recursive)
+		//Get the number of items that are on the stack (sequential)
+		//Check if the stacks have the same items in the same order (==)
+		//Stacking stack b over stack a
+		
+};
+#endif /*STACK_H_*/
